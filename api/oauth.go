a738c819b2e124d18be8fd6d76c20a59b70f5903
Aditya Balusu <balusu.aditya@gmail.com>
2017-03-12 22:53:34 +0530

    Redirecting the user to /close when the Facebook auth completes

--- api/oauth.go
+++ api/oauth.go
@@ -71,6 +71,6 @@ func (fb *FacebookCallback) Serve(ctx *iris.Context) {
 	ctx.Session().Set("id", user_model.Id)
 	ctx.Session().Set("short_id", user_model.ShortId)
 	fmt.Println("Saved user is :", user_model)
-	ctx.Redirect("/")
+	ctx.Redirect("/close")
 	return
 }
