308e794e704b75bc664b6e030ba2a22245602e3e
thekb <kbschandra@gmail.com>
2017-03-21 22:45:36 -0700

    adding java flatbuffer files

--- generate_messages.sh
+++ generate_messages.sh
@@ -4,7 +4,9 @@ echo "generating messages from flatbuffer files"
 
 echo "generating go bindings"
 rm -rf message/*.go
-flatc --grpc --go message/*.fbs
+flatc --go message/*.fbs
 echo "generating javascript bindings"
 rm -rf message/*.js
 flatc --js -o message/ message/*.fbs
+echo "generating java bindling"
+flatc --java message/*.fbs
