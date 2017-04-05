aa0f2ceaa5af817b35573b6024d35a2f178eabe0
thekb <kbschandra@gmail.com>
2017-04-04 23:31:06 -0700

    setting randon unique username

--- db/migrations/5-username.sql
+++ db/migrations/5-username.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 alter table users
-add column username text not null default '' unique;
+add column username text not null default md5(random()::text) unique;
 
 -- +migrate Down
 alter table usres
